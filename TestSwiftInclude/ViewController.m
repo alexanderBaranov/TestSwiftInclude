7c79d22f4cbb6189ff5d028270044dba2d7d729b
Александр <abaranov@diasoft.ru>
2018-01-23 23:49:10 +0300

    Add link from first view controller to second view controller.

--- TestSwiftInclude/ViewController.m
+++ TestSwiftInclude/ViewController.m
@@ -20,16 +20,25 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)setMessage:(NSString *)message {
+    _message = message;
+    NSLog(@"%@", _message);
+}
+
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if([segue.identifier isEqualToString:@"goToSwiftController"]) {
         ViewController2 *vc2 = segue.destinationViewController;
         vc2.message = @"Hellow, Swift!!!";
+        vc2.firstViewController = self;
     }
 }
 
--- TestSwiftInclude/ViewController2.swift
+++ TestSwiftInclude/ViewController2.swift
@@ -11,13 +11,26 @@ import UIKit
 @objc (ViewController2)
 class ViewController2: UIViewController {
     @objc var message: String?
+    @objc weak var firstViewController: ViewController?
     
     override func viewDidLoad() {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         guard let msg = message else {
             return
         }
-
+        
         print(msg)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        if let firstVC = firstViewController {
+            firstVC.message = "Hellow, objective c!!!"
+        }
+        
+        super.viewDidDisappear(animated)
     }
 }
